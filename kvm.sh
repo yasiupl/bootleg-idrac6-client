98c2b676f4704c948da82d0c03761eb314720b13
Marcin Jasiukowicz <contact@yasiu.pl>
2021-03-23 01:18:53 +0100

    initial commit

---
+++ kvm.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+download() {
+  jar=$1
+  path=$2
+  if [ ! -f "${path}/${jar}" ]; then
+
+	URI="Applications/dellUI/Java/release"
+
+	wget -O "${path}/${jar}" https://${IDRAC_HOST}/${URI}/${jar} --no-check-certificate
+
+	if [ ! $? -eq 0 ]; then
+	  echo -e "${RED}Failed to download ${jar}, please check your settings${NC}"
+	  sleep 2
+	  exit 2
+	fi
+  fi
+}
+
+echo -e "${GREEN}Starting${NC}"
+
+if [ -z "${IDRAC_HOST}" ]; then
+	echo -e "${RED}Please set a proper idrac host with IDRAC_HOST${NC}"
+	sleep 2
+	exit 1
+fi
+
+if [ -z "${IDRAC_USER}" ]; then
+	echo -e "${RED}Please set a proper idrac user with IDRAC_USER${NC}"
+	sleep 2
+	exit 1
+fi
+
+if [ -z "${IDRAC_PASSWORD}" ]; then
+	echo -e "${RED}Please set a proper idrac password with IDRAC_PASSWORD${NC}"
+	sleep 2
+	exit 1
+fi
+
+echo -e "${GREEN}Environment ok${NC}"
+
+cd /app
+mkdir lib
+touch cookies
+
+if [ -z "${COOKIE}" ]; then
+	echo -e "${GREEN}Obtaining session cookie${NC}"
+	COOKIE=$(curl -k --data "WEBVAR_USERNAME=${IDRAC_USER}&WEBVAR_PASSWORD=${IDRAC_PASSWORD}&WEBVAR_ISCMCLOGIN=0" https://${IDRAC_HOST}/Applications/dellUI/RPC/WEBSES/create.asp 2> /dev/null | grep SESSION_COOKIE | cut -d\' -f 4)
+fi
+
+echo -e "${GREEN}Cookie obtained:${NC}"
+echo $COOKIE
+echo "${COOKIE}" >> cookies
+
+echo -e "${GREEN}Downloading required files${NC}"
+
+## KVM App
+download JViewer.jar .
+
+## Platform specific libraries
+# platform="Win64.jar"
+# platform="Win32.jar"
+# platform="Linux_x86_32.jar"
+platform="Linux_x86_64.jar"
+
+download $platform lib
+
+# Extract libraries
+echo -e "${GREEN}Extracting libs${NC}"
+cd lib
+jar -xf $platform
+cd ..
+
+echo -e "${GREEN}Obtaining KVM launch parameters${NC}"
+
+args=$(curl -k --cookie Cookie=SessionCookie=${COOKIE} https://${IDRAC_HOST}/Applications/dellUI/Java/jviewer.jnlp | awk -F '[<>]' '/argument/ { print $3 }')
+
+echo $args
+
+echo -e "${GREEN}Running Java KVM Viewer${NC}"
+
+exec java -Djava.library.path=lib -jar JViewer.jar $args
+
+exit 1
